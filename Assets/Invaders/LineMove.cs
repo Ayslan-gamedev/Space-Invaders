c48da0801917437b931ace3c04f5c4b04bded508
Ayslan-gamedev <94746735+Ayslan-gamedev@users.noreply.github.com>
2022-12-21 16:57:23 -0300

    Style - rrefactoring invadres line move

--- Assets/Invaders/LineMove.cs
+++ Assets/Invaders/LineMove.cs
@@ -1,57 +1,41 @@
+using UnityEditor;
 using UnityEngine;
 
 public class LineMove : MonoBehaviour {
     [SerializeField] private protected GameObject[] invaders;
-    private float speed = 1f, speedDown = 0;
+    
+    private protected float speed = 1f, speedDown = 0;
     private protected const double LIMIT_X = 6;
 
-    int currentInvader = 0;
-    byte direction = 1;
-        byte changeDirection = 0;
+    private protected int currentInvader = 0;
+    private protected byte direction = 1, changeDirection = 0;
     
     private void Start() {
-        currentInvader = invaders.Length;
-    }
-
-    void Move() {
-
-        for(int i = 0; i < invaders.Length; i++) {
-            if(invaders[i] != null) {
-                invaders[i].transform.position += Vector3.right * speed; 
-                invaders[i].transform.position += Vector3.down * speedDown;
-
-             //   if(invaders[i].transform.position.x > LIMIT_X || invaders[i].transform.position.x < -LIMIT_X) changeDirection = 1;
-            }
+        switch(direction) {
+            case 0: currentInvader = 0; break;
+            case 1: currentInvader = invaders.Length; break;
         }
-        speedDown = 0;
-
-        //if(changeDirection == 1) {
-        //    speed *= -1;
-        //    speedDown = 1;
-        //}
     }
 
-    void Update() {
-        if(Input.GetKeyDown(KeyCode.L)) NeewMove();
+    private void Update() {
+        if(Input.GetKeyDown(KeyCode.L)) Move();
     }
 
-    private protected void NeewMove() {
+    private protected void Move() {
         do {
-            if(direction == 1) {
-                currentInvader--;
-                if(currentInvader < 0) {
-                    speedDown = 0;
-                    currentInvader = invaders.Length - 1;
-                    VerifyDirectionChange();
-                }
+            switch(direction) {
+                case 0: currentInvader++; break;
+                case 1: currentInvader--; break;
             }
-            else {
-                currentInvader++;
-                if(currentInvader > invaders.Length - 1) {
-                    speedDown = 0;
-                    currentInvader = 0;
-                    VerifyDirectionChange();
+
+            if(currentInvader > invaders.Length - 1 && direction == 0 || currentInvader < 0 && direction == 1) {
+                speedDown = 0;
+
+                switch(direction) {
+                    case 0: currentInvader = 0; break;
+                    case 1: currentInvader = invaders.Length - 1;  break;
                 }
+                VerifyDirectionChange();
             }
         } while(invaders[currentInvader].activeSelf != true);
 
@@ -62,18 +46,20 @@ public class LineMove : MonoBehaviour {
 
         void VerifyDirectionChange() {
             if(changeDirection == 1) {
-                if(direction == 1) {
-                    direction = 0;
-                    currentInvader = 0;
+                switch(direction) {
+                    case 0:
+                        direction = 1;
+                        currentInvader = invaders.Length - 1;
+                        break;
+                    case 1:
+                        direction = 0;
+                        currentInvader = 0;
+                        break;
                 }
-                else {
-                    direction = 1;
-                    currentInvader = invaders.Length - 1;
-                }
-
+                
                 speed *= -1;
-                changeDirection = 0;
                 speedDown = 1;
+                changeDirection = 0;
             }
         }
     }
