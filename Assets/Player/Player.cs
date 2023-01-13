0bcef7d7b5ebd141ca49fd02b4d505089df6e251
Ayslan Gamedev <94746735+Ayslan-gamedev@users.noreply.github.com>
2023-01-13 17:32:23 -0300

    add button to player shot

--- Assets/Player/Player.cs
+++ Assets/Player/Player.cs
@@ -14,7 +14,7 @@ public class Player : MonoBehaviour {
         float newPosition = transform.position.x + speed * Input.GetAxisRaw("Horizontal") * Time.deltaTime;
         if(newPosition > -LIMIT_X && newPosition < LIMIT_X) transform.position = new Vector2(newPosition, transform.position.y);
 
-        if(Input.GetKeyDown(KeyCode.V)) Instantiate(shotPreFab, transform.position, transform.rotation, shotTransform); ;
+        if(Input.GetKeyDown(KeyCode.Return)) Instantiate(shotPreFab, transform.position, transform.rotation, shotTransform); ;
 
         protectionRayCast = Physics2D.Raycast(transform.position + Vector3.up, Vector2.up, 0.5f, 1 << 7);
         Debug.DrawRay(transform.position + Vector3.up, Vector2.up, Color.blue, 0.5f);
